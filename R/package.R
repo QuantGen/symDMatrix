39b6d690cd053651b403da942015c9a842787b64
Alexander Grueneberg <alexander.grueneberg@googlemail.com>
2016-02-12 15:18:12 -0500

    Remove startup message for now.

--- R/package.R
+++ R/package.R
@@ -1,9 +1,3 @@
 #' @import ff methods parallel
 #' @importFrom bit physical physical<-
 NULL
-
-
-.onAttach <- function(libname, pkgname) {
-    packageStartupMessage("The symDMatrix package was supported by the National Institutes of Health (Grant: R01GM101219, R01GM099992).")
-    packageStartupMessage()
-}
