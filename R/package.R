f15bfe582f3d902d821d3138779315bdcbb7a0f4
Alexander Grueneberg <alexander.grueneberg@googlemail.com>
2017-02-28 16:49:16 -0500

    Add package documentation

--- R/package.R
+++ R/package.R
@@ -1,2 +1,7 @@
+#' A Package Providing Symmetric Matrices Assembled from Memory-Mapped Blocks.
+#'
+#' @docType package
+#' @name symDMatrix-package
+#' @aliases symDMatrix-package
 #' @import methods
 NULL
