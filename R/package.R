3f8ddc61f17d8a11bc569d70764117cdf1a8c111
Alexander Grueneberg <alexander.grueneberg@googlemail.com>
2017-03-20 11:16:09 -0400

    Link to class from package

--- R/package.R
+++ R/package.R
@@ -1,5 +1,6 @@
 #' A Package Providing Symmetric Matrices Assembled from Memory-Mapped Blocks.
 #'
+#' @seealso [symDMatrix-class] for the `symDMatrix` class.
 #' @docType package
 #' @name symDMatrix-package
 #' @aliases symDMatrix-package
